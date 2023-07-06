4cd40886392c2647dbfa7fa39bba77d7866be284
Damiyinliao <2911493102@qq.com>
2023-07-06 11:15:40 +0800

    feat: :rocket: 全局类型

--- types/global.d.ts
+++ types/global.d.ts
@@ -16,16 +16,16 @@ declare global {
   // Vite环境变量
   interface ViteEnv {
     VITE_USER_NODE_ENV: 'development' | 'production' | 'test';
-    VITE_GLOB_APP_TITLE: string;
+    VITE_GLOB_TITLE: string;
     VITE_PORT: number;
     VITE_OPEN: boolean;
     VITE_REPORT: boolean;
     VITE_BUILD_COMPRESS: 'gzip' | 'brotli' | 'gzip,brotli' | 'none';
-    VITE_BUILD_COMPRESS_DELETE_ORIGIN_FILE: boolean;
     VITE_DROP_CONSOLE: boolean;
     VITE_PWA: boolean;
     VITE_PUBLIC_PATH: string;
     VITE_API_URL: string;
     VITE_PROXY: [string, string][];
+    VITE_OUT_DIR: string;
   }
 }
